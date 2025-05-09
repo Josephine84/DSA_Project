9a00efa1b4cc120fade052e05e44ee1bc570ad88
Josephine84 <josephinekufuna@gmail.com>
2025-05-09 15:48:02 +0200

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,30 @@
+package studenteventmanagement;
+
+import studenteventmanagement.database.PostDatabase;
+
+public class Admin extends User {
+     
+    
+    public Admin(int id, String email, String password, String name) {
+        super(id, email, password, name);
+      
+    }
+    
+    
+       @Override
+    public void login(String email, String password) {
+       
+    }
+
+    @Override
+    public void logout() {
+       
+    }
+
+    public void createPost(Post post) {
+    PostDatabase.getInstance().savePost(post);
+}
+
+    
+   }
+    
